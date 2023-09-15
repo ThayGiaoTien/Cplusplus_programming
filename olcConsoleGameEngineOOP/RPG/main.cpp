c9440ff1ac829bf0a5a8b328ad6dd334fac6b3a3
MINHTIEN TRAN <68616669+ThayGiaoTien@users.noreply.github.com>
2023-09-15 10:57:42 +0700

    first initializations

--- olcConsoleGameEngineOOP/RPG/main.cpp
+++ olcConsoleGameEngineOOP/RPG/main.cpp
@@ -1,1 +1,39 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<utility>
 
+#include"olcConsoleGameEngineOOP.h"
+
+
+class RPG_Game : public olcConsoleGameEngineOOP
+{
+public:
+
+
+private:
+
+protected:
+	virtual bool OnUserCreate()
+	{
+
+		return true;
+	}
+	
+	virtual bool OnUserUpdate(float fElapsedTime)
+	{
+
+
+		return true;
+	}
+};
+
+
+int main()
+{
+	RPG_Game game;
+	game.ConstructConsole(320, 240, 8, 8);
+	game.Start();
+
+	return 0;
+}
